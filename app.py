c77757f9b399d177657697fda56f732ffa517064
Nitin John <nj@Mac.phub.net.cable.rogers.com>
2025-03-19 15:28:57 -0400

    Parameter name bug fix

--- app.py
+++ app.py
@@ -17,17 +17,22 @@ def home():
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    req =  request.get_json()
-    intent = req.get('queryResult', {}).get('intent', {}).get('displayName','')
+    req =  req.get_json()
+    # app.logger.debug(f"Received request: {req}")
+    intent = req.get('queryResult', {}).get('intent', {}).get('displayName', '')
     parameters = req.get('queryResult', {}).get('parameters', {})
 
     if intent == "Salary estimation intent":
-        job_role = parameters.get('job_role', "").title()
-        response_text = f"The average salary for a {job_role} is {salary_data.get(job_role, 'not available at the moment')}."
+        job_title = parameters.get("job_title", "").title()
+        if job_title and job_title in salary_data:
+            response_text = f"The average salary for a {job_title} is {salary_data[job_title]}."
+        else:
+            response_text = "I'm sorry, I couldn't find any relevant salary information for that role."
     else:
         response_text = "Sorry, I didn't understand that. Can you please try again"
 
     return jsonify({'fulfillmentText': response_text})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
